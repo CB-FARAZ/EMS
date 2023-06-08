dd50dd3e4bd90f70a7ffc2b29272802f3c46d8dd
CB-FARAZ <cbgul786@gmail.com>
2023-06-08 18:04:38 +0500

    Middleware

---
+++ Demo/app/Http/Controllers/employee/SearchController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\employee;
+
+use App\Http\Controllers\Controller;
+
+class SearchController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
